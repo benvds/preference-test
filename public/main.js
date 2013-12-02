d4a0a13a6082b45be19d66cdcd73210a1c14038e
benvds <ben@pomodoro.nl>
2013-12-02 15:39:02 +0100

    added swipebox, text & images

--- public/main.js
+++ public/main.js
@@ -1,4 +1,6 @@
 jQuery(function($) {
+    $(".swipebox").swipebox();
+
     $('.preference').on('click', function() {
         var value = $(this).data('value');
         $('.questionaire-value').val(value);
